337eb926d1c3ef86995aa7d4c771b3607634d092
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2021-02-21 20:51:50 +0300

    common event classes

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,20 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerEvent implements Serializable {
+
+    private static final long serialVersionUID = -8063133939587132163L;
+
+    private BeerDto beerDto;
+
+}
