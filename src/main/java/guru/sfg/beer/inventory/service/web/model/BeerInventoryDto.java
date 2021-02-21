48541be93fd748ca92ef3c86c99d050fc2eeacc7
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2021-02-21 20:53:06 +0300

    serializable

--- src/main/java/guru/sfg/beer/inventory/service/web/model/BeerInventoryDto.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/model/BeerInventoryDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -15,7 +16,10 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class BeerInventoryDto {
+public class BeerInventoryDto implements Serializable {
+
+    private static final long serialVersionUID = 5197963334649684262L;
+
     private UUID id;
     private OffsetDateTime createdDate;
     private OffsetDateTime lastModifiedDate;
